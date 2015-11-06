3b7770c4b68319aa92e83c14f89b853878f53d43
rachelaskin <rachel.askin@gmail.com>
2015-11-06 16:46:38 -0500

    Help with jQuery needed
    Line 20: re: rachel_ingredients.css not being able to be styled until
    the jQuery is working.
    Line 52: Scroll functionality stopped working although I haven’t edited
    that section of jQuery at all.
    Line 304: re: Add Ingredient button functionality.
    Line 345: re: adding a text area when “Add another step” button is
    clicked.
    Line 353: connecting to Google Sheet (although this is the last concern
    in terms of priority).

--- Day_14/Rachel_project/js/freelancer.js
+++ Day_14/Rachel_project/js/freelancer.js
@@ -1,4 +1,4 @@
-/*!
+    /*!
  * Start Bootstrap - Freelancer Bootstrap Theme (http://startbootstrap.com)
  * Code licensed under the Apache License v2.0.
  * For details, see http://www.apache.org/licenses/LICENSE-2.0.
@@ -15,7 +15,7 @@ $(function() {
     });
 });
 
-// Floating label headings for the contact form
+/*// Floating label headings for the contact form
 $(function() {
     $("body").on("input propertychange", ".floating-label-form-group", function(e) {
         $(this).toggleClass("floating-label-form-group-with-value", !! $(e.target).val());
@@ -24,7 +24,7 @@ $(function() {
     }).on("blur", ".floating-label-form-group", function() {
         $(this).removeClass("floating-label-form-group-with-focus");
     });
-});
+});*/
 
 // Highlight the top nav as scrolling occurs
 $('body').scrollspy({
@@ -35,3 +35,104 @@ $('body').scrollspy({
 $('.navbar-collapse ul li a').click(function() {
     $('.navbar-toggle:visible').click();
 });
+
+
+
+
+// Add new ingredient when submit button is clicked
+$('#newIngredient').on('submit', addNewIngredient);
+
+// Remove ingredient when remove button is clicked
+$('#ingredients-added').on('click', '.remove', removeIngredient)
+
+// When an edit link is clicked, edit the ingredient
+$('#ingredients-added').on('click', '.edit', editIngredient);
+
+// When an item editor is submitted, save the changes
+$('#ingredients-added').on('submit', '.editor', saveIngredient);
+
+// When a user leaves an item editor form, save the changes
+$('#ingredients-added').on('blur', '.editor', saveIngredient);
+
+//Add new, remove, edit, and save ingredients
+    function addNewIngredient (event) {
+        event.preventDefault();
+
+        var newIngredient = $('#newIngredient').val();
+
+        $('#ingredients-added').append('<li><div class="btn btn-success btn-lg">' + newIngredient + '</div><a class="edit">Edit</a><a class="remove">Remove</a></li>');
+        
+    }
+
+    function removeIngredient(event) {
+        event.preventDefault();
+
+        $(this).parent().remove();
+
+    }
+
+    function editIngredient(event){
+        event.preventDefault();
+
+        // Get the text of the to-do item; it's a sibling of the clicked link
+        var ingredientText = $(this).siblings('.ingredients').html();
+
+        // Get the parent <li>
+        var listIngredient = $(this).parent();
+
+        // Replace the parent <li> contents with an edit form
+        listIngredient.html('<form class="editor"><input value="' + ingredientText + '"></form>');
+
+        // Focus the keyboard on the input that was just added
+        listIngredient.find('input').focus();
+    }
+
+    function saveIngredient(event){
+        event.preventDefault();
+
+        var ingredientText = $(this).children('input').val();
+
+        var listIngredient = $(this).parent();
+
+        listIngredient.html('<span class="item">' + ingredientText + '</span><a class="edit">Edit</a><a class="remove">Remove</a>');)
+    }
+
+
+// Add an additional text area for more recipe instructions
+        function getNumberOfSteps () {
+
+            count = $('#form-group col-xs-12').length;
+            console.log(count);
+        }
+
+        function addNewStep(){
+
+            var stepNumber = getNumberOfSteps() + 1;
+
+            var newStepHtml = '<div class="row control-group">' +
+                            '<div class="form-group col-xs-12 floating-label-form-group controls">' +
+                                '<label>Message</label>' +
+                                '<textarea rows="5" class="form-control" placeholder="Step ' + stepNumber + '" id="message" required data-validation-required-message="Please enter an instruction."></textarea>' +
+                                '<p class="help-block text-danger"></p>' +
+                            '</div>' +
+                        '</div>';
+            // append newStepHtml to the screen
+            $('#form-group col-xs-12').append('newStepHtml' + 1);
+
+        }
+
+        $("#add-step").on("click", addNewStep);
+
+        // function postToGoogle (){
+        //     $.ajax({
+        //         url: "https://docs.google.com/forms/d/1Jy5nKG-NJxymmeIWDIa4yXlkkvb7IJQMonT2kY0_cWQ/viewform"
+        //         data: {
+        //             "entry.679142677": recipe1,
+        //             " ": etc
+        //             },
+        //             type: "POST",
+        //             dataType: "xml",
+        //         {});
+        // };
+
+        // $
