238fc4b00d13e00144f749c38f3803937514fe44
rachelaskin <rachel.askin@gmail.com>
2015-11-09 19:13:48 -0500

    FINAL PROJECT
    WAHOO IT’S DONE :)

--- Day_14/Rachel_project/js/freelancer.js
+++ Day_14/Rachel_project/js/freelancer.js
@@ -41,19 +41,19 @@ $('.navbar-collapse ul li a').click(function() {
 //     event.preventDefault();
 
 // Add new ingredient when submit button is clicked
-$('newIngredient').on('submit', addNewIngredient);
+$('#newIngredient').on('submit', addNewIngredient);
     
 // Remove ingredient when remove button is clicked
 $('#ingredients-added').on('click', '.remove', removeIngredient);
     
 // When an edit link is clicked, edit the ingredient
-$('#ingredients-added').on('click', '.edit', editIngredient);
+// $('#ingredients-added').on('click', '.edit', editIngredient);
     
 // When an item editor is submitted, save the changes
-$('#ingredients-added').on('submit', '.editor', saveIngredient);
+$('.editor').on('submit', saveIngredient);
     
 // When a user leaves an item editor form, save the changes
-$('#ingredients-added').on('blur', '.editor', saveIngredient);
+$('.editor').on('blur', saveIngredient);
 
 //Add new, remove, edit, and save ingredients
     function addNewIngredient (event) {
@@ -61,9 +61,13 @@ $('#ingredients-added').on('blur', '.editor', saveIngredient);
 
         var newIngredient = $('#ingredientName').val();
 
-        $('#ingredients-added').append('<li><div class="btn btn-success btn-lg">' + newIngredient + '</div><a class="edit">Edit</a><a class="remove">Remove</a></li>');
-        // append('<li><div class="btn btn-success btn-lg">' + newIngredient + '</div><a class="edit">Edit</a><a class="remove">Remove</a></li>');
-        
+        var newMeasurement = $('#select-measurement option:selected').text();
+
+        var newUnit = $('#select-unit option:selected').text();
+
+        $('#ingredients-added').append('<li><div class="ingredients">' + newMeasurement + ' ' + newUnit + ' ' + newIngredient + '</div>&nbsp; &nbsp; &nbsp; <a class="remove">Remove</a></li>');
+    // $('#ingredients-added').append('<li><div class="ingredients">' + newMeasurement + ' ' + newUnit + ' ' + newIngredient + '</div>&nbsp; &nbsp; <a class="edit">Edit</a>&nbsp; &nbsp; <a class="remove">Remove</a></li>');
+        $('#newIngredient').trigger("reset");
     }
 
     function removeIngredient(event) {
@@ -73,30 +77,33 @@ $('#ingredients-added').on('blur', '.editor', saveIngredient);
 
     }
 
-    function editIngredient(event){
-        event.preventDefault();
+    // function editIngredient(event){
+    //     event.preventDefault();
 
-        // Get the text of the to-do item; it's a sibling of the clicked link
-        var ingredientText = $(this).siblings('.ingredients').html();
+    //     // Get the text of the item; it's a sibling of the clicked link
+    //     var ingredientText = $(this).siblings('.ingredients').html();
+    //     console.log(ingredientText);
 
-        // Get the parent <li>
-        var listIngredient = $(this).parent();
+    //     // Get the parent <li>
+    //     var listIngredient = $(this).parent();
 
-        // Replace the parent <li> contents with an edit form
-        listIngredient.html('<form class="editor"><input value="' + ingredientText + '"></form>');
+    //     // Replace the parent <li> contents with an edit form
+    //     listIngredient.html('<form class="editor"><input value="' + ingredientText + '"></form>');
 
-        // Focus the keyboard on the input that was just added
-        listIngredient.find('input').focus();
-    }
+    //     // Focus the keyboard on the input that was just added
+    //     listIngredient.find('input').focus();
+    // }
 
     function saveIngredient(event){
         event.preventDefault();
+        // console.log("test");
 
-        var ingredientText = $(this).children('input').val();
+        var ingredientText = $(this).val();
+        console.log(ingredientText);
 
         var listIngredient = $(this).parent();
 
-        listIngredient.html('<span class="item">' + ingredientText + '</span><a class="edit">Edit</a><a class="remove">Remove</a>');
+        listIngredient.html('<div class="ingredients">' + ingredientText + '</div><a class="edit">Edit</a><a class="remove">Remove</a>');
     }
 
 
